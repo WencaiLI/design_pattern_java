412df22b7c00ec1540397187914b5ae06355222d
liwencai <1491269442@qq.com>
2023-06-23 11:26:42 +0800

    add strategy design pattern

---
+++ src/b_design_pattern/d_strategy_design_pattern/ConcurrentExternalSort.java
@@ -0,0 +1,12 @@
+package b_design_pattern.d_strategy_design_pattern;
+
+/**
+ * @author liwencai
+ * @since 2023/6/23
+ */
+public class ConcurrentExternalSort implements ISortAlg{
+    @Override
+    public void sort(String filePath) {
+
+    }
+}
---
+++ src/b_design_pattern/d_strategy_design_pattern/ISortAlg.java
@@ -0,0 +1,9 @@
+package b_design_pattern.d_strategy_design_pattern;
+
+/**
+ * @author liwencai
+ * @since 2023/6/23
+ */
+public interface ISortAlg {
+    void sort(String filePath);
+}
